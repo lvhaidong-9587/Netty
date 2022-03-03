d016e92ce3ecdf34cf1f0459f4c577e9939227f6
lvhaidong-9587 <luhaidong@gamil.com>
2022-03-03 17:13:06 +0800

    2022.3.1 提交java 阻塞io和异步io;netty 的oio和nio 服务器demo;netty ByteBuf缓冲区写入和读取的相关demo; 2022.3.1 更新启动类; 2022.3.2 更新读索引、写索引移动和利用ByteBufProcessor查找特定字符 2022.3.3 更新ByteBuf更加详细的读写操作，包括缓冲区的复制，不移动索引的读写等 2022.3.3 更新了codec编解码框架,SSL加密支持,Http 通道初始化，聚合通道初始化,Https 通道初始化

--- src/main/java/demo/netty/https/HttpsInitializerDemo.java
+++ src/main/java/demo/netty/https/HttpsInitializerDemo.java
@@ -2,6 +2,13 @@ package demo.netty.https;
 
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslHandler;
+
+import javax.net.ssl.SSLEngine;
 
 /**
  * PROJECT_NAME: Netty
@@ -11,8 +18,25 @@ import io.netty.channel.ChannelInitializer;
  * Description: https 通道初始化
  */
 public class HttpsInitializerDemo extends ChannelInitializer<Channel> {
+
+    private final SslContext context;
+    private final boolean client;
+
+    public HttpsInitializerDemo(SslContext context,boolean client){
+        this.context = context;
+        this.client = client;
+    }
+
     @Override
     protected void initChannel(Channel channel) throws Exception {
-        
+        ChannelPipeline pipeline = channel.pipeline();
+        SSLEngine engine = context.newEngine(channel.alloc());
+        pipeline.addFirst("ssl",new SslHandler(engine));
+
+        if(client){
+            pipeline.addLast("codec",new HttpClientCodec());
+        }else {
+            pipeline.addLast("codec",new HttpServerCodec());
+        }
     }
 }
