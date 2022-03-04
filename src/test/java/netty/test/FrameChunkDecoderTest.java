43ed4a63e69c623ba0a947ab5b9d028df78576bc
lvhaidong-9587 <luhaidong@gamil.com>
2022-03-04 17:27:18 +0800

    2022.3.1 提交java 阻塞io和异步io;netty 的oio和nio 服务器demo;netty ByteBuf缓冲区写入和读取的相关demo; 2022.3.1 更新启动类; 2022.3.2 更新读索引、写索引移动和利用ByteBufProcessor查找特定字符 2022.3.3 更新ByteBuf更加详细的读写操作，包括缓冲区的复制，不移动索引的读写等 2022.3.3 更新了codec编解码框架,SSL加密支持,Http 通道初始化，聚合通道初始化,Https 通道初始化 2022.3.4 更新了对websocket的支持，心跳的发送、对具有分割符的帧的处理，和按长度来处理帧，channel的引导程序：客户端、服务端、channel引导、和channelOption的属性设置 2022.3.4 更新了通道的单元测试代码

--- src/test/java/netty/test/FrameChunkDecoderTest.java
+++ src/test/java/netty/test/FrameChunkDecoderTest.java
@@ -1,5 +1,16 @@
 package netty.test;
 
+import demo.netty.test.FrameChunkDecoder;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.codec.TooLongFrameException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * PROJECT_NAME: Netty
  *
@@ -8,4 +19,46 @@ package netty.test;
  * Description: 解码入站测试
  */
 public class FrameChunkDecoderTest {
+
+    @Test
+    public void testFramesDecoded(){
+        //创建一个 ByteBuf,并向它写入 9 字节
+        ByteBuf buf = Unpooled.buffer();
+        for(int i = 0;i < 9;i++){
+            buf.writeByte(i);
+        }
+
+        ByteBuf input = buf.duplicate();
+        //创建一个 EmbeddedChannel , 并向它提供一个字节为 3 的帧
+        EmbeddedChannel channel = new EmbeddedChannel(
+          new FrameChunkDecoder(3)
+        );
+
+        //向它写入 2 个字节，断言 它会产生一个新帧
+        assertTrue(channel.writeInbound(input.readBytes(2)));
+        try{
+            //写入一个 4 字节大小的帧，尝试捕获定义的异常
+            channel.writeInbound(input.readBytes(4));
+            //上一步没有抛出异常则说明测试失败
+            Assert.fail();
+        }catch (TooLongFrameException e){
+            //
+        }
+        //写入剩下的 2 个字节，并断言将会产生一个有效帧
+        assertTrue(channel.writeInbound(input.readBytes(3)));
+        //标记该channel为已完成状态
+        assertTrue(channel.finish());
+
+        //读取产生的消息，并验证值
+        ByteBuf read = channel.readInbound();
+        assertEquals(buf.readSlice(2),read);
+        read.release();
+
+        read = channel.readInbound();
+        assertEquals(buf.skipBytes(4).readSlice(3),read);
+        read.release();
+        buf.release();
+    }
+
+
 }
