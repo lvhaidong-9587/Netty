eddcf184caad01d45e6223b07c1942d2ef74f23a
lvhaidong-9587 <luhaidong@gamil.com>
2022-03-08 17:30:41 +0800

    2022.3.1 提交java 阻塞io和异步io;netty 的oio和nio 服务器demo;netty ByteBuf缓冲区写入和读取的相关demo; 2022.3.1 更新启动类; 2022.3.2 更新读索引、写索引移动和利用ByteBufProcessor查找特定字符 2022.3.3 更新ByteBuf更加详细的读写操作，包括缓冲区的复制，不移动索引的读写等 2022.3.3 更新了codec编解码框架,SSL加密支持,Http 通道初始化，聚合通道初始化,Https 通道初始化 2022.3.4 更新了对websocket的支持，心跳的发送、对具有分割符的帧的处理，和按长度来处理帧，channel的引导程序：客户端、服务端、channel引导、和channelOption的属性设置 2022.3.4 更新了通道的单元测试代码 2022.3.7 更新了chat部分代码 2022.3.8 更新了基于tcp的聊天系统服务,和部分udp广播服务

--- src/main/java/demo/netty/charUdp/LogEventBroadcaster.java
+++ src/main/java/demo/netty/charUdp/LogEventBroadcaster.java
@@ -1,11 +1,55 @@
 package demo.netty.charUdp;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+
+import java.io.File;
+import java.net.InetSocketAddress;
+
 /**
  * PROJECT_NAME: Netty
  *
  * @author: lhd
  * 2022/3/8 0008 17:03
- * Description:
+ * Description: logEvent服务引导程序
  */
 public class LogEventBroadcaster {
+    private final EventLoopGroup eventLoopGroup;
+    private final Bootstrap bootstrap;
+    private final File file;
+
+    public LogEventBroadcaster(InetSocketAddress address,File file){
+        eventLoopGroup = new NioEventLoopGroup();
+        bootstrap = new Bootstrap();
+        bootstrap.group(eventLoopGroup)
+                //引导该 NioDatagramChannel（无连接的）
+                .channel(NioDatagramChannel.class)
+                //保活
+                .option(ChannelOption.SO_KEEPALIVE,true)
+                .handler(new LogEventEncoder(address));
+        this.file = file;
+    }
+
+    public void run()throws Exception{
+        //绑定 channel
+        Channel channel = bootstrap.bind(0).sync().channel();
+        //用来指文件字节的指针
+        long pointer = 0;
+        //启动主处理循环
+        for(;;){
+            long len = file.length();
+            if(len < pointer){
+                //文件被重置
+                //如果有必要，将文件指针设置到该文件的最后一个字节
+                pointer = len;
+            }else if(len > pointer){
+                
+            }
+        }
+    }
 }
